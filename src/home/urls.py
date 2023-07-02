8683a3d619c4c4294c906a1bbcb39b0a9d30da0d
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-02 21:56:55 +0500

    writing hello world

--- src/home/urls.py
+++ src/home/urls.py
@@ -18,8 +18,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
 
 if settings.DEBUG:
