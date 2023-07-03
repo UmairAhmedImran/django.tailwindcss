71b7828d170b4f239b2641ecaadd2197cbc75c64
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-03 22:59:09 +0500

    adding flowbite to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/templates/**/*.{html,js}", './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
 /** if color did not render add followind code 
