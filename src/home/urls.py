7180016023dee83d209c80518aa007fccb6b1537
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-07 20:58:44 +0500

    adding a new template for login and register page

--- src/home/urls.py
+++ src/home/urls.py
@@ -23,6 +23,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('login', views.login, name= 'login'),
+    path('register', views.register, name= 'register'),
 ]
 
 if settings.DEBUG:
--- src/home/views.py
+++ src/home/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home/home.html", {})
+
+def login(request):
+    return render(request, "home/login.html", {})
+
+def register(request):
+    return render(request, "home/register.html", {})
