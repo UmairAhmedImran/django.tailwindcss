5c485bfff45abadc4f2c201d84c39785478331d3
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-07-12 21:40:46 +0500

    modifying models and changing some UI settings

--- src/home/models.py
+++ src/home/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Registeration(models.Model):
     email = models.EmailField(max_length=254)
     password = models.TextField(max_length=100)
+    verification=models.BooleanField(default=False)
